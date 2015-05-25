95453e31be807fc01be3a171fe7a837bfce7e1e5
Clayton Fidelis <clayton.fidelis@live.com>
2015-05-25 17:52:21 -0300

    Pequenas modificações

--- index.php
+++ index.php
@@ -5,46 +5,13 @@
 ?>
 <html lang="pt-br">
 <head>
-	<meta charset="UTF-8">
-	<link rel="stylesheet" type="text/css" href="css/style.css">
-	<script src="js/jquery-1.7.2.js" type="text/javascript"></script>
-	<script src="js/jquery.validate.js"type="text/javascript"></script>
-	<script type="text/javascript">
-		$(document).ready(function(){
-			$('#loginForm').validate({
-				rules:{
-					username:{
-						required:true,
-						rangelength: [5,32]
-					},
-					password:{
-						required:true,
-						rangelength: [3,32]
-					}
-				},
-				messages:{
-					username:{
-						required:"Este campo é obrigatório",
-						rangelength: "O nome de usuário deve conter entre 5 e 32 caracteres"
-					},
-					password:{
-						required:"Este campo é obrigatório",
-						rangelength: "A sua senha deve conter entre 3 e 32 caracteres"
-					}
-
-				}
-			});
-		});
-	</script>
+	<?php include 'include/head.php'; ?>
 	<title>Página inicial</title>
 </head>
 <body>
-	<div class="topo">	</div>
-	<div class="menu"><a href="index.php">Inicio</a> || <a href="index.php">Logar</a> || <a href="index.php">Registrar</a> || <?php if(logged_in()) echo '<a href="logout.php">Sair</a>'?></div>
+		<?php include 'include/topo.php'; ?>
 
-	<div class="container">
 		<div class="painelEsquerda">
-
 		</div>
 
 		<div class="painelDireita">
@@ -57,12 +24,9 @@
 			}
 			include 'include/user_count.php';			
 		?>
-		</div>
-		
+		</div>	
 	</div>
 
-	<div class="footer">
-
-	</div>
+	<?php include 'include/footer.php'; ?>
 </body>
 </html>
--- register.php
+++ register.php
@@ -7,94 +7,27 @@
 
 <html lang="pt-br">
 <head>
-	<meta charset="UTF-8">
-	<link rel="stylesheet" type="text/css" href="css/style.css">
-	<script src="js/jquery-1.7.2.js" type="text/javascript"></script>
-	<script src="js/jquery.validate.js"type="text/javascript"></script>
-	<script type="text/javascript">
-		$(document).ready(function(){
-
-			$('#registerForm').validate({
-				rules:{
-					username:{
-						required:true,
-						rangelength: [5,32]
-					},
-					first_name:{
-						required:true,
-						rangelength: [3,32]
-					},
-					last_name:{
-						required:true,
-						rangelength: [3,32]
-					},
-					password:{
-						required:true,
-						rangelength: [5,32]
-					},
-					password_again:{
-						required: true,
-					    equalTo: "#password"
-					},
-					email:{
-						required: true,
-						email: true
-					}
-				},
-				messages:{
-					username:{
-						required:"Este campo é obrigatório",
-						rangelength: "O nome de usuário deve conter entre 5 e 32 caracteres"
-					},
-					password:{
-						required:"Este campo é obrigatório",
-						rangelength: "A sua senha deve conter entre 3 e 32 caracteres"
-					},
-					first_name:{
-						required:"Este campo é obrigatório",
-						minlength: "O nome deve conter no minímo 3 caracteres"
-					},
-					last_name:{
-						required:"Este campo é obrigatório",
-						minlength: "O sobrenome deve conter no mínimo 3 carácteres"
-					},
-					password_again:{
-						required: "Este campo é obrigatório",
-						equalTo: "As senhas não estão iguais"
-					},
-					email:{
-						required: "Este campo é obrigatório",
-						email: "Insira um endereço de email válido."
-					}
-				}
-			});
-		});
-	</script>
-	<title>Criar nova conta</title
+	<?php include 'include/head.php'; ?>
+	<title>Criar nova conta</title>
 </head>
 <body>
-	<div class="topo">	</div>
-	<div class="menu"><a href="index.php">Inicio</a> || <a href="index.php">Logar</a> || <a href="index.php">Registrar</a> || <?php if(logged_in()) echo '<a href="logout.php">Sair</a>'?></div>
+	<?php include 'include/topo.php'; ?>
 
-	<div class="container">
 		<div class="painelEsquerda">
-		
-		<form action="" method="post" id="registerForm">
-			Usuário: <br> <input type="text" name="username"> <br>
-			Nome: <br> <input type="text" name="first_name"> <br>
-			Sobrenome: <br> <input type="text" name="last_name"> <br>
-			Email: <br> <input type="email" name="email"> <br>
-			Senha: <br> <input type="password" id="password" name="password"> <br>
-			Repita a senha: <br> <input  type="password" name="password_again"> <br>
-			<?php include 'registervalidation.php'; ?> <br>
-			<input type="submit" name="Register" value="Registrar"> <br>
-
-
+			<form action="" method="post" id="registerForm">
+				Usuário: <br> <input type="text" name="username"> <br>
+				Nome: <br> <input type="text" name="first_name"> <br>
+				Sobrenome: <br> <input type="text" name="last_name"> <br>
+				Email: <br> <input type="email" name="email"> <br>
+				Senha: <br> <input type="password" id="password" name="password"> <br>
+				Repita a senha: <br> <input  type="password" name="password_again"> <br>
+				<?php include 'registervalidation.php'; ?> <br>
+				<input type="submit" name="Register" value="Registrar"> <br>
+			</form>
 		</div>
-
+		
 		<div class="painelDireita">
 		<?php
-			
 			if(logged_in()==true){
 				include 'include/loggedin.php';
 			}
@@ -106,10 +39,7 @@
 		</div>
 		
 	</div>
-
-	<div class="footer">
-
-	</div>
+	<?php include 'include/footer.php'; ?>
 </body>
 </html>
 
